751eb00fd0781a0b88d5b04a53120fa210fae7fb
yuetong3yu <yuetong3yu@gmail.com>
2021-12-02 18:10:30 +0800

    ⛰️ all is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,30 @@
+import { useEffect, useRef } from 'react';
+
 export const App = () => {
-  return <h1>Hello World!!</h1>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    async function getCamera() {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+    }
+    getCamera();
+  }, []);
+
+  return (
+    <>
+      <h1>Camera: </h1>
+      <video
+        ref={videoRef}
+        onLoadedMetadata={() => {
+          videoRef.current?.play();
+        }}
+      />
+    </>
+  );
 };
